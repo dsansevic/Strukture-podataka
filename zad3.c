555adf40331815dcf4f2fa55a7bb75b7359189d3
Jelena Sarić <115426213+jsaric21@users.noreply.github.com>
2022-11-04 12:03:58 +0100

    Add files via upload

--- zad3.c
+++ zad3.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAX 50
+#define MAX_LINE 1024
 #define FILE_ERROR -1
 #define EXIT_SUCCESS 0
 
@@ -17,116 +19,123 @@ typedef struct _osoba
 	Pozicija Next;
 }osoba;
 
+
+int Menu();
 int UnosPodataka(Pozicija);
+int PostaviPokazivace(Pozicija, Pozicija);
 int UnosNaPocetak(Pozicija);
 int UnosNaKraj(Pozicija);
 int IspisListe(Pozicija);
 int BrisiElement(char*, Pozicija);
 int UnosIspred(Pozicija, char*);
 int UnosIza(Pozicija, char*);
+int UpisUDatoteku(Pozicija, char*);
+int CitanjeIzDatoteke(Pozicija, char*);
+int SortiraniUnos(Pozicija);
+int BrisiSve(Pozicija);
 Pozicija PronadiElement(char*, Pozicija);
 Pozicija TraziPrethodnog(char*, Pozicija);
 
 
-
 int main()
 {
-	int a = 0, b = 0, c = 0, i = 0;
+	int a = 0, b = 0, i = 0;
+	char izbor;
+	char m = 'K';
 	char prez[MAX] = { 0 };
+	char file[MAX_LINE] = { 0 };
+	char file2[MAX_LINE] = { 0 };
 	Pozicija Head = NULL;
-
 	Head = (Pozicija)malloc(sizeof(osoba));
 	Head->Next = NULL;
 
-	printf("UNOS OCJENA\n");
-	printf("Za unos na pocetak liste upisite 1\nZa unos na kraj liste upisite 2\n");
-	scanf("%d", &a);
-	if (a == 1)
+	printf("VEZANA LISTA, odaberite opciju:\n");
+	do
 	{
-		printf("Odabrali ste unos na pocetak liste. Mozete poceti s unosom podataka\n");
-		UnosNaPocetak(Head);
+		Menu();
+		scanf(" %c", &izbor);
+		izbor = toupper(izbor);
 
-		printf("Zelite li unijeti jos jednu osobu na pocetak liste? \nUkoliko zelite unesite 1.\n");
-		scanf("%d", &b);
-		if (b == 1)
+		switch (izbor)
 		{
-			printf("Koliko osoba zelite unijeti?\n");
-			scanf("%d", &c);
-			for (i = 0; i < c; i++)
+		case 'A':
+			scanf("%d", &a);
+			for (i = 0; i < a; i++)
 				UnosNaPocetak(Head);
-		}
-	}
-
-	else if (a == 2)
-	{
-		printf("Odabrali ste unos na kraj liste. Mozete poceti s unosom podataka\n");
-		UnosNaKraj(Head);
-
-		printf("Zelite li unijeti jos jednu osobu na kraj liste? \nUkoliko zelite unesite 1.\n");
-		scanf("%d", &b);
-		if (b == 1)
-		{
-			printf("Koliko osoba zelite unijeti?\n");
-			scanf("%d", &c);
-			for (i = 0; i < c; i++)
+			break;
+		case 'B':
+			scanf("%d", &a);
+			for (i = 0; i < a; i++)
 				UnosNaKraj(Head);
-		}
-	}
-	else
-		printf("Krivi unos");
-
-	/*printf("\nVasa lista:\n");
-	IspisListe(Head->Next);
-
-	printf("Unesite prezime iza kojeg ide\n");
-	scanf(" %s", prez);
-	UnosIza(Head, prez);
-	IspisListe(Head->Next);
-
-	printf("Unesite prezime za ispred\n");
-	scanf(" %s", prez);
-	UnosIspred(Head->Next, prez);
-	IspisListe(Head->Next);*/
-
-
-
-	printf("\nZelite li obrisati korisnike?\nAko zelite, upisite 1\n");
-	scanf("%d", &a);
-	if (a == 1)
-	{
-		printf("Koliko korisnika zelite izbrisati?\n");
-		scanf("%d", &b);
-
-		for (i = 0; i < b; i++)
-		{
-
-			printf("Unesite prezime korisnika:\n");
+			break;
+		case 'C':
+			IspisListe(Head->Next);
+			break;
+		case 'D':
+			printf("Prezime trazene osobe:\n");
+			scanf(" %s", prez);
+			PronadiElement(prez, Head->Next);
+			if (PronadiElement(prez, Head->Next) == EXIT_SUCCESS)
+				printf("Korisnik uspjesno pronaden!\n");
+			else
+			{
+				printf("Korisnik nije pronaden!\n");
+				return FILE_ERROR;
+			}
+			break;
+		case 'E':
+			printf("Prezime osobe koju zelite izbrisati:\n");
 			scanf(" %s", prez);
 			BrisiElement(prez, Head);
+			IspisListe(Head->Next);
+			break;
+		case 'F':
+			printf("Prezime osobe nakon koje unosite:\n");
+			scanf(" %s", prez);
+			UnosIza(Head->Next, prez);
+			IspisListe(Head->Next);
+			break;
+		case 'G':
+			printf("Prezime osobe ispred koje unosite:\n");
+			scanf(" %s", prez);
+			UnosIspred(Head, prez);
+			IspisListe(Head->Next);
+			break;
+		case 'H':
+			SortiraniUnos(Head);
+			IspisListe(Head->Next);
+			break;
+		case 'I':
+			printf("Unesite ime datoteke:\n");
+			scanf(" %s", file);
+			UpisUDatoteku(Head->Next, file);
+			IspisListe(Head->Next);
+			break;
+		case 'J':
+			printf("Unesite ime datoteke:\n");
+			scanf(" %s", file2);
+			CitanjeIzDatoteke(Head->Next, file2);
+			//IspisListe(Head->Next);
+			break;
+		default:
+			printf("Krivi unos slova\n");
 
 		}
-		printf("\nVasa nova lista:\n");
-		IspisListe(Head->Next);
-	}
-	else
-		printf("Krivi unos");
+	} while (izbor != m);
 
-	printf("\nZelite li pronaci odredenog korisnika?\nAko zelite, upisite 1.\n");
-	scanf("%d", &a);
-	if (a == 1)
-	{
-		printf("Upisite prezime korisnika kojeg zelite pronaci\n");
-		scanf(" %s", prez);
-		PronadiElement(prez, Head);
-	}
-	else
-		printf("Krivi unos");
-	
-	//free(Head);
+	if (izbor == m)
+		printf("Kraj programa");
+	BrisiSve(Head);
 
 	return EXIT_SUCCESS;
 }
 
+int PostaviPokazivace(Pozicija P, Pozicija Q)
+{
+	Q->Next = P->Next;
+	P->Next = Q;
+	return EXIT_SUCCESS;
+}
 
 int UnosNaPocetak(Pozicija P)
 {
@@ -139,11 +148,7 @@ int UnosNaPocetak(Pozicija P)
 		return FILE_ERROR;
 	}
 	UnosPodataka(Q);
-
-	Q->Next = P->Next;
-	P->Next = Q;
-
-	//free(Q);
+	PostaviPokazivace(P, Q);
 
 	return EXIT_SUCCESS;
 }
@@ -163,23 +168,21 @@ int UnosNaKraj(Pozicija P)
 	while (P->Next != NULL)
 		P = P->Next;
 
-	Q->Next = P->Next;
-	P->Next = Q;
-
-	//free(Q);
+	PostaviPokazivace(P, Q);
 	return EXIT_SUCCESS;
 }
 
 Pozicija PronadiElement(char* x, Pozicija P)
 {
-	while (P != NULL && strcmp(P->prezime, x))
-		P = P->Next;
-
 	if (P == NULL)
 	{
 		printf("Element ne postoji");
 		return NULL;
 	}
+
+	while (P != NULL && strcmp(P->prezime, x))
+		P = P->Next;
+
 	printf("Podaci korisnika:\nIme: %s\nPrezime: %s\nGodina: %d\n", P->ime, P->prezime, P->godina);
 
 	return P;
@@ -253,7 +256,7 @@ int UnosIspred(Pozicija P, char* prez)
 {
 	Pozicija Pret = NULL;
 	Pret = TraziPrethodnog(prez, P);
-	
+
 	if (Pret == NULL)
 		return FILE_ERROR;
 	else
@@ -261,10 +264,8 @@ int UnosIspred(Pozicija P, char* prez)
 		Pozicija Q = NULL;
 		Q = (Pozicija)malloc(sizeof(osoba));
 		UnosPodataka(Q);
-		Q->Next = Pret->Next;
-		Pret->Next = Q;
+		PostaviPokazivace(P, Q);
 	}
-	//free(Pret);
 
 	return EXIT_SUCCESS;
 }
@@ -272,12 +273,108 @@ int UnosIspred(Pozicija P, char* prez)
 int UnosIza(Pozicija P, char* prez)
 {
 	Pozicija X = NULL;
-	X = PronadiElement(prez, X);
+	X = PronadiElement(prez, P);
 
-	UnosPodataka(X);
-	X->Next = P->Next;
-	P->Next = X;
-	//free(X);
+	if (X == NULL)
+	{
+		printf("Neuspjesna pretraga");
+		return FILE_ERROR;
+	}
+	UnosNaPocetak(X);
+
+	return EXIT_SUCCESS;
+}
+
+int UpisUDatoteku(Pozicija P, char* file)
+{
+	FILE* fp = NULL;
+	fp = fopen(file, "w");
+	if (fp == NULL)
+	{
+		printf("Neuspjesno otvaranje datoteke!");
+		return FILE_ERROR;
+	}
+	while (P != NULL)
+	{
+		fprintf(fp, "Student:%-20s %-20s, godina %4d\n", P->ime, P->prezime, P->godina);
+		P = P->Next;
+	}
+	fclose(fp);
+	return EXIT_SUCCESS;
+}
+
+int CitanjeIzDatoteke(Pozicija P, char* file)
+{
+	FILE* fp = NULL;
+	fp = fopen(file, "r");
+	char buff[MAX_LINE] = { 0 };
+	if (fp == NULL)
+	{
+		printf("Neuspjesno otvaranje datoteke!");
+		return FILE_ERROR;
+	}
+	
+	while (fgets(buff, MAX_LINE, fp) != NULL)
+		puts(buff);
+
+	fclose(fp);
+	return EXIT_SUCCESS;
+}
+
+
+int SortiraniUnos(Pozicija P)
+{
+	Pozicija j = NULL, prev_j = NULL, temp = NULL, end = NULL;
+	while (P->Next != end)
+	{
+		prev_j = P;
+		j = P->Next;
+		while (j->Next != end)
+		{
+			if (strcmp(j->prezime, j->Next->prezime) > 0) //prvi veci od drugoga po ASCII
+			{
+				temp = j->Next;
+				prev_j->Next = temp;
+				j->Next = temp->Next;
+				temp->Next = j;
+
+				j = temp;
+			}
+
+			prev_j = j;
+			j = j->Next;
+		}
+		end = j;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+int BrisiSve(Pozicija P)
+{
+	Pozicija temp = NULL;
+	while (P->Next != NULL)
+	{
+		temp = P->Next;
+		P->Next = temp->Next;
+		free(temp);
+	}
+	return EXIT_SUCCESS;
+}
+
+int Menu()
+{
+	printf("A) UNOS NA POCETAK\t\n");
+	printf("B) UNOS NA KRAJ\t\n");
+	printf("C) ISPIS LISTE\t\n");
+	printf("D) TRAZENJE ELEMENTA\t\n");
+	printf("E) BRISANJE ELEMENTA\t\n");
+	printf("F) DODAVANJE NOVOG ELEMENTA IZA\t\n");
+	printf("G) DODAVANJE NOVOG ELEMENTA ISPRED\t\n");
+	printf("H) SORTIRANJE\t\n");
+	printf("I) UPIS U DATOTEKU\t\n");
+	printf("J) CITANJE IZ DATOTEKE\t\n");
+	printf("Ako ste izabrali A) ili B), upisite koliki broj korisnika zelite.\t\n");
 
 	return EXIT_SUCCESS;
 }
